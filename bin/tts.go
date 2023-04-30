b556cd301250af6432f7929caba7cb458f299e72
sh1ma <in9lude@gmail.com>
2023-04-30 23:02:49 +0900

    feat: ttsサンプルを追加

---
+++ bin/tts.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	voicevoxcorego "github.com/sh1ma/voicevoxcore.go"
+)
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("usage:\n\tgo run tts.go [ text ]")
+		os.Exit(127)
+	}
+	text := os.Args[1]
+
+	core := voicevoxcorego.NewVoicevoxCore()
+	initializeOptions := voicevoxcorego.NewVoicevoxInitializeOptions(0, 0, false, "/Users/sh1ma/Projects/voicevoxcore.go/open_jtalk_dic_utf_8-1.11")
+	err := core.Initialize(initializeOptions)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = core.LoadModel(1)
+	if err != nil {
+		fmt.Println(err)
+	}
+	ttasOptions := voicevoxcorego.NewVoicevoxTtsOptions(false, true)
+	result, err := core.Tts(text, 1, ttasOptions)
+	if err != nil {
+		fmt.Println(err)
+	}
+	f, _ := os.Create("out.wav")
+	_, err = f.Write(result)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
