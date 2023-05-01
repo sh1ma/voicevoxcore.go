bf72592f063a48b495f8848c9b6ec8bf93b8e696
sh1ma <in9lude@gmail.com>
2023-05-01 17:02:22 +0900

    chore: godocコメントを書いた

--- wrapper.go
+++ wrapper.go
@@ -12,11 +12,13 @@ type VoicevoxCore struct {
 	initialized bool
 }
 
+// VoicevoxCore のコンストラクタ関数
 func NewVoicevoxCore() (core VoicevoxCore) {
 	core = VoicevoxCore{rawCore: &RawVoicevoxCore{}}
 	return
 }
 
+// C APIを通じてVoicevox_coreを初期化する関数
 func (r *VoicevoxCore) Initialize(options VoicevoxInitializeOptions) (err error) {
 	if r.initialized {
 		err = errors.New("Already initialized")
@@ -27,6 +29,7 @@ func (r *VoicevoxCore) Initialize(options VoicevoxInitializeOptions) (err error)
 	return
 }
 
+// 音声合成モデルをロードする関数
 func (r *VoicevoxCore) LoadModel(speakerID int) (err error) {
 	id := C.uint(speakerID)
 	code := r.rawCore.VoicevoxLoadModel(id)
@@ -36,13 +39,15 @@ func (r *VoicevoxCore) LoadModel(speakerID int) (err error) {
 	return
 }
 
+// Text to Speechを実行する関数。実行結果はwavファイルフォーマットのバイト列。
+// Sample: https://github.com/sh1ma/sample-tts
 func (r *VoicevoxCore) Tts(text string, speakerID int, options VoicevoxTtsOptions) (slicebytes []byte, err error) {
 	ctext := C.CString(text)
 	cspeakerID := C.uint(speakerID)
 
+	// TODO: このあたりを関数にまとめる
 	var size int
 	data := make([]*C.uchar, 1)
-	// cbyte := C.getCByte()
 	datap := (**C.uchar)(&data[0])
 	defer r.rawCore.VoicevoxWavFree(*datap)
 
@@ -60,6 +65,8 @@ func (r *VoicevoxCore) Tts(text string, speakerID int, options VoicevoxTtsOption
 	return
 }
 
+// Audio Queryを基に音声合成を実行する関数。実行結果はwavファイルフォーマットのバイト列。
+// Sample: https://github.com/sh1ma/sample-synthesis
 func (r *VoicevoxCore) Synthesis(
 	audioQuery string,
 	speakerID int,
@@ -68,9 +75,9 @@ func (r *VoicevoxCore) Synthesis(
 	ctext := C.CString(audioQuery)
 	cspeakerID := C.uint(speakerID)
 
+	// HACK: 煩雑なコード。Tts()のTODOと一緒の関数にまとめる
 	var size int
 	data := make([]*C.uchar, 1)
-	// cbyte := C.getCByte()
 	datap := (**C.uchar)(&data[0])
 	defer r.rawCore.VoicevoxWavFree(*datap)
 
