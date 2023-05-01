f336bd078305087e546afff5cd462dd535d2a89d
sh1ma <in9lude@gmail.com>
2023-05-01 14:04:15 +0900

    feat: synthesisのexamples追加

---
+++ examples/simple-synthesis/synthesis.go
@@ -0,0 +1,46 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	voicevoxcorego "github.com/sh1ma/voicevoxcore.go"
+)
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("usage:\n\tgo run tts.go [ audioquery_json_path ]")
+		os.Exit(127)
+	}
+	jsonPath := os.Args[1]
+
+	file, err := os.ReadFile(jsonPath)
+	audioQuery := string(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	core := voicevoxcorego.NewVoicevoxCore()
+	initializeOptions := voicevoxcorego.NewVoicevoxInitializeOptions(0, 0, false, "./open_jtalk_dic_utf_8-1.11")
+	err = core.Initialize(initializeOptions)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = core.LoadModel(1)
+	if err != nil {
+		fmt.Println(err)
+	}
+	ttsOptions := voicevoxcorego.NewVoicevoxSynthesisOptions(false)
+	result, err := core.Synthesis(audioQuery, 1, ttsOptions)
+	if err != nil {
+		fmt.Println(err)
+	}
+	f, _ := os.Create("out.wav")
+	_, err = f.Write(result)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
